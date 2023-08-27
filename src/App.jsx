5bf834b52040e8913b58afcd52df85b28d14e82c
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-27 19:49:14 -0300

    Se establece Navbar.jsx para todas las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-
-import { Route, Routes } from 'react-router-dom';
-import './App.css'
-import Home from './components/home/Home';
-import Navbar from './components/navbar/Navbar';
-import Favorites from './components/favorites/Favorites';
-import Synopsis from './synopsis/Synopsis';
-import Henry_m2 from './components/henry_m2/Henry_m2';
-import Tecnologies from './components/tecnologies/Tecnologies';
-import About from './components/about/About';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/home/Home";
+import Navbar from "./components/navbar/Navbar";
+import Favorites from "./components/favorites/Favorites";
+import Synopsis from "./synopsis/Synopsis";
+import Henry_m2 from "./components/henry_m2/Henry_m2";
+import Tecnologies from "./components/tecnologies/Tecnologies";
+import About from "./components/about/About";
+import Error404 from "./components/error404/Error404";
 
 function App() {
-
-
   return (
     <div>
+      <Navbar />
       <Routes>
-        <Route path="/navbar" element={<Navbar />} />
         <Route path="/" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
         <Route path="/synopsis" element={<Synopsis />} />
         <Route path="/henry_m2" element={<Henry_m2 />} />
         <Route path="/tecnologies" element={<Tecnologies />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </div>
   );
 }
 
-export default App
+export default App;
