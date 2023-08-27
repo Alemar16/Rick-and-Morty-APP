4bba277016fe62d61fb327ad3625da13e49b4072
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-27 17:18:37 -0300

    se instala dependencias de Bootswatch

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
+// bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+import "bootswatch/dist/spacelab/bootstrap.min.css";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// bootstrap
-import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
