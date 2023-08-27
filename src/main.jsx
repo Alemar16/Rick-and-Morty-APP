1e10773c49c63e4b75958660dd6eeea9576ed14b
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-27 17:32:28 -0300

    Se importa y configura BrowserRouter en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 // bootstrap
 import "bootstrap/dist/css/bootstrap.css";
 import "bootswatch/dist/spacelab/bootstrap.min.css";
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
+);
