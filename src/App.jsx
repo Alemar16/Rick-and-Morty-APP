5defaf5db9dcf2c8dca91c69c3d929e61ac7f706
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-27 18:52:08 -0300

    Se agregan rutas a las secciones del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Home from './components/home/Home';
 import Navbar from './components/navbar/Navbar';
+import Favorites from './components/favorites/Favorites';
+import Synopsis from './synopsis/Synopsis';
+import Henry_m2 from './components/henry_m2/Henry_m2';
+import Tecnologies from './components/tecnologies/Tecnologies';
+import About from './components/about/About';
 
 function App() {
 
@@ -10,8 +15,13 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/navbar' element={<Navbar />} />
+        <Route path="/navbar" element={<Navbar />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/synopsis" element={<Synopsis />} />
+        <Route path="/henry_m2" element={<Henry_m2 />} />
+        <Route path="/tecnologies" element={<Tecnologies />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
