1b2c6c40cc0d86a501ddacc3a2c9360c00a5dc48
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-27 22:05:58 -0300

    Cambios para prueba lineas 19 y 20

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,16 @@ import Henry_m2 from "./components/henry_m2/Henry_m2";
 import Tecnologies from "./components/tecnologies/Tecnologies";
 import About from "./components/about/About";
 import Error404 from "./components/error404/Error404";
+import Login from "./components/login/Login";
+import Signup from "./components/login/SignUp";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
         <Route path="/synopsis" element={<Synopsis />} />
