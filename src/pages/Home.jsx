08e6b75b3750ffac8755e5e54c920875156873a8
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-28 17:10:17 -0300

    Se crea contexto de prueba en Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Rick and Morty</h1>
+      <div className="container">
+        <button className="btn btn-primary">Botón de Bootstrap</button>
+      </div>
+    </div>
+  );
+}
+
+export default Home
