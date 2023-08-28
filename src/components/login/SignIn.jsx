26c1724385dcdcac2b52dedfd9a957fa8d263bf9
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-27 23:34:22 -0300

    anterior login. se modifica nombre a SignIn.jsx

---
+++ src/components/login/SignIn.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { auth } from "../firebase/firebase.js"; // Importa el objeto de autenticación de Firebase
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+
+  const handleSignin = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Inicia sesión con Firebase usando el correo electrónico y la contraseña
+      await signInWithEmailAndPassword(auth, email, password);
+      // Si el inicio de sesión fue exitoso, puedes realizar alguna acción adicional o redirigir al usuario a una página específica
+      // Redirigir al usuario a la ruta "/"
+      navigate("/");
+    } catch (error) {
+      // Maneja los errores de inicio de sesión, si los hay
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Sign In</h2>
+      <form onSubmit={handleSignin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Sign in</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
