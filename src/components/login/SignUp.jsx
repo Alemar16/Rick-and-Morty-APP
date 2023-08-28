afb3a414956e06b001d69bd6287aa1c9922b96ef
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-27 23:33:41 -0300

    Se agrega ruta pots ingreso a "/"

--- src/components/login/SignUp.jsx
+++ src/components/login/SignUp.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { auth } from "../firebase/firebase.js";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
-const Signup = () => {
+const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const Navigate = useNavigate();
 
   const handleSignup = async (e) => {
     e.preventDefault();
@@ -13,6 +15,8 @@ const Signup = () => {
       // Crea una cuenta con Firebase usando el correo electrónico y la contraseña
       await createUserWithEmailAndPassword(auth, email, password);
       // Si la creación de la cuenta fue exitosa, puedes realizar alguna acción adicional o redirigir al usuario a una página específica
+      // Redirigir al usuario a la ruta "/"
+      Navigate("/");
     } catch (error) {
       // Maneja los errores de creación de cuenta, si los hay
       console.log(error);
@@ -21,7 +25,7 @@ const Signup = () => {
 
   return (
     <div>
-      <h2>Signup</h2>
+      <h2>Sign Up</h2>
       <form onSubmit={handleSignup}>
         <input
           type="email"
@@ -35,10 +39,10 @@ const Signup = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Signup</button>
+        <button type="submit">Sign up</button>
       </form>
     </div>
   );
 };
 
-export default Signup;
+export default SignUp;
