f9a972997a6d624152f285fe5710d3ba4d786640
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-27 23:35:01 -0300

    Se crea Login.jsx definitivo

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,42 +1,18 @@
-import { useState } from "react";
-import { auth } from "../firebase/firebase.js"; // Importa el objeto de autenticación de Firebase
-import {signInWithEmailAndPassword} from "firebase/auth";
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-
-    try {
-      // Inicia sesión con Firebase usando el correo electrónico y la contraseña
-      await signInWithEmailAndPassword(auth,email, password);
-      // Si el inicio de sesión fue exitoso, puedes realizar alguna acción adicional o redirigir al usuario a una página específica
-    } catch (error) {
-      // Maneja los errores de inicio de sesión, si los hay
-      console.log(error);
-    }
-  };
-
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Login</button>
-      </form>
+    <div className="container">
+      <h2 className="text-center">Login</h2>
+      <div className="row">
+        <div className="col">
+          <SignUp />
+        </div>
+        <div className="col">
+          <SignIn />
+        </div>
+      </div>
     </div>
   );
 };
