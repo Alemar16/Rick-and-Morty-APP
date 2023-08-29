eef4db60bd75e7af561f2e212c0c5ae438ff8668
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-28 23:17:08 -0300

    Estilos y funcionamiento del componente

--- src/components/login/SignUp.jsx
+++ src/components/login/SignUp.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showInputs, setShowInputs] = useState(false);
   const Navigate = useNavigate();
 
   const handleSignup = async (e) => {
@@ -23,25 +24,44 @@ const SignUp = () => {
       console.log(error);
     }
   };
+  const handleSignUpInClick = () => {
+    setShowInputs(true);
+  }
+
+  const handleGoBack = () => {
+    setShowInputs(false);
+  }
 
   return (
-    <div>
-      <h2>Sign Up</h2>
-      <form onSubmit={handleSignup}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Sign up</button>
-      </form>
+   <div className="mt-5">
+      <h1>Create an account</h1>
+      {!showInputs ? (
+        <button className="btn btn-secondary" onClick={handleSignUpInClick}>
+          Sign Up
+        </button>
+      ) : (
+        <>
+          <div className="d-flex align-items-center">
+            <h2 className="me-2">Sign Up</h2>
+            <i onClick={handleGoBack} className="bi bi-reply-fill"></i>
+          </div>
+          <form onSubmit={handleSignup}>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Sign Up</button>
+          </form>
+        </>
+      )}
     </div>
   );
 };
