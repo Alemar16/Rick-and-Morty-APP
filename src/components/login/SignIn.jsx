c7114396d9545f24983fd52589fb442a956052b6
Armando Martinez <alemar.martinez16@gmail.com>
2023-08-28 23:17:02 -0300

    Estilos y funcionamiento del componente

--- src/components/login/SignIn.jsx
+++ src/components/login/SignIn.jsx
@@ -1,49 +1,64 @@
 import { useState } from "react";
-import { auth } from "../firebase/firebase.js"; // Importa el objeto de autenticación de Firebase
+import { auth } from "../firebase/firebase.js";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showInputs, setShowInputs] = useState(false);
   const navigate = useNavigate();
-
 
   const handleSignin = async (e) => {
     e.preventDefault();
 
     try {
-      // Inicia sesión con Firebase usando el correo electrónico y la contraseña
       await signInWithEmailAndPassword(auth, email, password);
-      // Si el inicio de sesión fue exitoso, puedes realizar alguna acción adicional o redirigir al usuario a una página específica
       console.log("Usuario iniciado");
-      // Redirigir al usuario a la ruta "/"
       navigate("/home");
     } catch (error) {
-      // Maneja los errores de inicio de sesión, si los hay
       console.log(error);
     }
   };
 
+  const handleSignInClick = () => {
+    setShowInputs(true);
+  };
+
+  const handleGoBack = () => {
+    setShowInputs(false);
+  };
+
   return (
-    <div>
-      <h2>Sign In</h2>
-      <form onSubmit={handleSignin}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Sign in</button>
-      </form>
+    <div className="mt-5">
+      <h1>You have an account?</h1>
+      {!showInputs ? (
+        <button className="btn btn-secondary" onClick={handleSignInClick}>
+          Sign In
+        </button>
+      ) : (
+        <>
+          <div className="d-flex align-items-center">
+            <h2 className="me-2">Sign In</h2>
+            <i onClick={handleGoBack} className="bi bi-reply-fill"></i>
+          </div>
+          <form onSubmit={handleSignin}>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Sign in</button>
+          </form>
+        </>
+      )}
     </div>
   );
 };
