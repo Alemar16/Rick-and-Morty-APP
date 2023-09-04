acac1b3f148f2ccba986bd2effc1be04ab92b855
Armando Martinez <alemar.martinez16@gmail.com>
2023-09-04 00:17:11 -0300

    Se modifica estructura de Error404.jsx

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Error404 = () => {
+  const navigate = useNavigate()
   return (
-    <div>Error404</div>
+    <div className="container">
+      <h1>Error404</h1>
+      <button
+        className='btn btn-primary'
+        onClick={() => navigate(-1)}
+      >Return
+      </button>
+    </div>
   )
 }
 
