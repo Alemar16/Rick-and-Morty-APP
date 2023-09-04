907f0b43a53e3039fe30b9593e25af410156169b
Armando Martinez <alemar.martinez16@gmail.com>
2023-09-04 00:13:53 -0300

    Se restructura el componente Logout

--- src/components/login/Logout.jsx
+++ src/components/login/Logout.jsx
@@ -1,24 +1,26 @@
-import React from "react";
-import { signOut } from "firebase/auth";
-import { auth } from "../firebase/firebase.js";
 import { useNavigate } from "react-router-dom";
+import { UserAuth } from "../../context/AuthContext";
 
-const Logout = () => {
+export const Logout = () => {
   const navigate = useNavigate();
-
-  const logoutHandler = async () => {
-    await signOut(auth);
-    console.log("Usuario deslogueado");
-    navigate("/");
+  const { user, logout } = UserAuth();
+  const cerrarSesion = async () => {
+    try {
+      await logout();
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <div>
-      <button className="btn btn-secondary" onClick={logoutHandler}>
+    <div className="">
+      <h6 className="text-secondary col ">Welcome {user.displayName}</h6>
+      <button className="btn btn-secondary col " onClick={cerrarSesion}>
         Logout
       </button>
     </div>
   );
 };
-
 export default Logout;
+
