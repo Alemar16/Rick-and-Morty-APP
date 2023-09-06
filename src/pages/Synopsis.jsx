9b5b259e6d853a9e25450348e96754499ee30f23
Armando Martinez <armandomartinez16@hotmail.com>
2023-09-06 08:20:44 -0300

    se aplica funcionalidad de consultas a api

--- src/pages/Synopsis.jsx
+++ src/pages/Synopsis.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import CharacterList, {
+  SynopsisContext,
+} from "../components/characterlist/CharacterList";
 
 const Synopsis = () => {
-  return (
-    <div>Synopsis</div>
-  )
-}
+  const [page, setPage] = useState(() => {
+    const storedPage = localStorage.getItem("page");
+    return storedPage ? parseInt(storedPage) : 1;
+  });
 
-export default Synopsis
+  useEffect(() => {
+    localStorage.setItem("page", page.toString());
+  }, [page]);
+
+  return (
+    <div
+      className="container-fluid"
+      style={{ maxHeight: "100vh", marginTop: "60px" }}
+    >
+      <h1 className="text-center display-1 py-4 mt-5 mb-3 fw-bold fs-1 text-secondary">
+        Synopsis
+      </h1>
+      <h2 className="text-center mt-5 mb-3 fw-bold fs-1 text-secondary">
+        Rick and Morty
+      </h2>
+
+      <SynopsisContext.Provider value={{ page, setPage }}>
+        <CharacterList />
+      </SynopsisContext.Provider>
+    </div>
+  );
+};
+
+export default Synopsis;
