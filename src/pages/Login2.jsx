ff7164014d95642d8e594cdb4fbfe332343de263
Armando Martinez <armandomartinez16@hotmail.com>
2023-09-07 16:39:46 -0300

    se agrega validaciones de password

--- src/pages/Login2.jsx
+++ src/pages/Login2.jsx
@@ -23,11 +23,35 @@ const Login2 = () => {
     }
   };
 
-  //para validar checbox de remember me
+  //para validar checbox de Remember me
   const [rememberMe, setRememberMe] = useState(false);
   const handleCheckboxChangeRememberMe = (event) => {
     setRememberMe(event.target.checked);
   };
+
+  //para logica de password y password repeat
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+  const handleRepeatPasswordChange = (event) => {
+    setRepeatPassword(event.target.value);
+  }
+  const validatePasswords = () => {
+    if (password === repeatPassword) {
+      // Las contraseñas coinciden, realizar el registro
+      alert("Success registration");
+      // Aquí puedes agregar la lógica para enviar los datos del formulario o realizar otras acciones necesarias
+    } else {
+      // Las contraseñas no coinciden, mostrar un mensaje de error
+      setErrorMessage(
+        "Passwords do not match. Please try again."
+      );
+    }
+  };
+
 
   return (
     <div
@@ -42,9 +66,9 @@ const Login2 = () => {
       }}
     >
       {/* Pills navs */}
-      <ul className="nav nav-pills nav-justified mb-3 " id="ex1" role="tablist">
+      <ul className="nav nav-pills nav-justified mb-3 mt-3 " id="ex1" role="tablist">
         {/* Login */}
-        <li className="nav-item" role="presentation">
+        <li className="nav-item " role="presentation">
           <a
             className={`nav-link ${activeTab === "login" ? "active" : ""}`}
             onClick={() => handleTabClick("login")}
@@ -171,7 +195,7 @@ const Login2 = () => {
             <div className="text-center">
               <button
                 type="submit"
-                className="btn btn-primary btn-block mb-3"
+                className="btn btn-primary btn-block "
                 style={{ margin: "0 auto" }}
               >
                 Sign in
@@ -194,31 +218,26 @@ const Login2 = () => {
           /* style={{maxHeight: "400px"}} */
         >
           <form onSubmit={handleFormSubmit}>
-            <div className="text-center mb-3">
-              <p>Sign in with:</p>
-
-              {/* Facebook */}
-              <button
-                className="btn btn-dark btn-floating mx-2"
-                title="Facebook"
-              >
-                <img
-                  src={facebookIcon}
-                  alt="Facebook"
-                  style={{ width: "35px" }}
-                />
-              </button>
-              {/* Google */}
-              <button className="btn btn-dark btn-floating mx-2" title="Google">
-                <img src={googleIcon} alt="Google" style={{ width: "35px" }} />
-              </button>
-              {/* Github */}
-              <button className="btn btn-dark btn-floating mx-2" title="Github">
-                <img src={githubIcon} alt="Github" style={{ width: "35px" }} />
-              </button>
+            <div className="text-center mb-3 mt-3">
+              <p>
+                "Ready to get started? Sign up now!"
+              </p>
             </div>
 
-            <p className="text-center">or:</p>
+            {/* identification Name input and Username input */}
+            {/* Email input */}
+            <div className="input-group flex-nowrap mb-4">
+              <span className="input-group-text" id="addon-wrapping">
+                <i className="fa-solid fa-user-large"></i>
+              </span>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                aria-label="Username"
+                aria-describedby="addon-wrapping"
+              />
+            </div>
 
             {/* Email input */}
             <div className="input-group flex-nowrap mb-4">
@@ -232,10 +251,13 @@ const Login2 = () => {
                 aria-label="Username"
                 aria-describedby="addon-wrapping"
               />
-            </div>
 
+
+
+
+            </div>
             {/* Password input */}
-            <div className="input-group flex-nowrap mb-4">
+            <div className="input-group flex-nowrap mb-2">
               <span className="input-group-text" id="addon-wrapping">
                 <i className="fa-solid fa-key"></i>
               </span>
@@ -245,8 +267,30 @@ const Login2 = () => {
                 placeholder="Password"
                 aria-label="Password"
                 aria-describedby="addon-wrapping"
+                value={password}
+                onChange={handlePasswordChange}
               />
             </div>
+            {/* Repeat Password input */}
+            <div className="input-group flex-nowrap mb-4">
+              <span className="input-group-text" id="addon-wrapping">
+                <i className="fa-solid fa-key"></i>
+              </span>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Repeat Password"
+                aria-label="Password"
+                aria-describedby="addon-wrapping"
+                value={repeatPassword}
+                onChange={handleRepeatPasswordChange}
+              />
+            </div>
+
+
+
+            
+
 
             {/* Checkbox */}
             <div className="form-check d-flex justify-content-center mb-4">
@@ -263,17 +307,21 @@ const Login2 = () => {
                 I have read and agree to the terms
               </label>
             </div>
-
             {/* Submit button */}
+            {errorMessage && 
+              <p>{errorMessage}</p>
+            }
             <div className="text-center">
               <button
                 type="submit"
-                className="btn btn-primary btn-block mb-3"
+                className="btn btn-primary btn-block "
                 style={{ margin: "0 auto" }}
+                onClick={validatePasswords}
               >
                 Sign in
               </button>
             </div>
+            {/* Exit button */}
             <div className="text-right y ml-auto mb-1" title="Exit">
               <i className="fa-solid fa-arrow-right-from-bracket fa-lg"></i>
             </div>
@@ -286,6 +334,41 @@ const Login2 = () => {
 };
 
 export default Login2;
+
+
+
+//  <div className="row mb-4">
+//    <div className="col">
+   
+//      <div className="input-group flex-nowrap mb-1 me-1">
+//        <span className="input-group-text" id="addon-wrapping">
+//          <i class="fa-solid fa-user-large"></i>
+//        </span>
+//        <input
+//          type="text"
+//          className="form-control"
+//          placeholder="Name"
+//          aria-label="Username"
+//          aria-describedby="addon-wrapping"
+//        />
+//      </div>
+//    </div>
+//    <div className="col">
+   
+//      <div className="input-group flex-nowrap mb-1">
+//        <span className="input-group-text" id="addon-wrapping">
+//          <i class="fa-solid fa-user-ninja"></i>
+//        </span>
+//        <input
+//          type="text"
+//          className="form-control"
+//          placeholder="Username"
+//          aria-label="Username"
+//          aria-describedby="addon-wrapping"
+//        />
+//      </div>
+//    </div>
+//  </div>;
 
 //========================================================================
 // import { useState } from "react";
@@ -514,25 +597,25 @@ export default Login2;
 
 //             <p className="text-center">or:</p>
 
-//             {/* Name input */}
-//             {/* <div className="form-outline mb-4">
-//               <input type="text" id="registerName" className="form-control" />
-//               <label className="form-label" htmlFor="registerName">
-//                 Name
-//               </label>
-//             </div> */}
+            // {/* Name input */}
+            // {/* <div className="form-outline mb-4">
+            //   <input type="text" id="registerName" className="form-control" />
+            //   <label className="form-label" htmlFor="registerName">
+            //     Name
+            //   </label>
+            // </div> */}
 
-//             {/* Username input */}
-//             {/* <div className="form-outline mb-4">
-//               <input
-//                 type="text"
-//                 id="registerUsername"
-//                 className="form-control"
-//               />
-//               <label className="form-label" htmlFor="registerUsername">
-//                 Username
-//               </label>
-//             </div> */}
+            // {/* Username input */}
+            // {/* <div className="form-outline mb-4">
+            //   <input
+            //     type="text"
+            //     id="registerUsername"
+            //     className="form-control"
+            //   />
+            //   <label className="form-label" htmlFor="registerUsername">
+            //     Username
+            //   </label>
+            // </div> */}
 
 //             {/* Email input */}
 //             <div className="input-group flex-nowrap mb-4">
