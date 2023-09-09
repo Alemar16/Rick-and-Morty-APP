f40b144fba5f6c19f157afe4358da686c3e47a5d
Armando Martinez <alemar.martinez16@gmail.com>
2023-09-09 14:06:20 -0300

    solo metodo de seguridad de rutas PrivateRoutes

--- src/routers/PrivateRoutes.jsx
+++ src/routers/PrivateRoutes.jsx
@@ -5,11 +5,28 @@ import { Navigate } from "react-router-dom";
 const PrivateRoutes = ({ children }) => {
   const { user } = UserAuth();
   if (!user) {
-    return <Navigate to="/home" />;
+    return <Navigate to="/" />;
   }
   return children;
 };
+
 PrivateRoutes.propTypes = {
-  children: propTypes.node.isRequired
-}
+  children: propTypes.node,
+};
 export default PrivateRoutes;
+
+// import propTypes from "prop-types";
+// import { UserAuth } from "../context/AuthContext";
+// import { Navigate } from "react-router-dom";
+
+// const PrivateRoutes = ({ children }) => {
+//   const { user } = UserAuth();
+//   if (!user) {
+//     return <Navigate to="/home" />;
+//   }
+//   return children;
+// };
+// PrivateRoutes.propTypes = {
+//   children: propTypes.node.isRequired
+// }
+// export default PrivateRoutes;
--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -1,5 +1,4 @@
-import propTypes from "prop-types";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import PrivateRoutes from "./PrivateRoutes";
 // pages
 import Navbar from "../components/navbar/Navbar";
@@ -14,29 +13,23 @@ import About from "../pages/About";
 import Error404 from "../pages/Error404";
 import { UserAuth } from "../context/AuthContext";
 import Login2 from "../pages/Login2";
-const RequireAuth = ({ children }) => {
-  const { user } = UserAuth();
-    return user ? children : <Navigate to="/login" />;
-};
-  RequireAuth.propTypes = {
-    children: propTypes.node.isRequired,
-  };
+
 export const MyRoutes = () => {
   const { user } = UserAuth();
-  
+
   return (
     <>
       {user ? <Navbar /> : null}
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login2 />} /> 
+        <Route path="/login" element={<Login2 />} />
 
         <Route
           path="/home"
           element={
-            <RequireAuth>
+            <PrivateRoutes>
               <Home />
-            </RequireAuth>
+            </PrivateRoutes>
           }
         />
         <Route
@@ -83,4 +76,91 @@ export const MyRoutes = () => {
       </Routes>
     </>
   );
-}
+};
+
+// import propTypes from "prop-types";
+// import { Routes, Route, Navigate } from "react-router-dom";
+// import PrivateRoutes from "./PrivateRoutes";
+// // pages
+// import Navbar from "../components/navbar/Navbar";
+// import LandingPage from "../pages/LandingPage";
+// import Home from "../pages/Home";
+// import Favorites from "../pages/Favorites";
+// import Synopsis from "../pages/Synopsis";
+// import Henry_m2 from "../pages/Henry_m2";
+// import Tecnologies from "../pages/Tecnologies";
+// import About from "../pages/About";
+// import Error404 from "../pages/Error404";
+// import { UserAuth } from "../context/AuthContext";
+// //import Login2 from "../pages/Login2";
+// import Login from "../pages/Login";
+// const RequireAuth = ({ children }) => {
+//   const { user } = UserAuth();
+//   return user ? children : <Navigate to="/login" />;
+// };
+// RequireAuth.propTypes = {
+//   children: propTypes.node.isRequired,
+// };
+// export const MyRoutes = () => {
+//   const { user } = UserAuth();
+
+//   return (
+//     <>
+//       {user ? <Navbar /> : null}
+//       <Routes>
+//         <Route path="/" element={<LandingPage />} />
+//         {/* <Route path="/login" element={<Login />} /> */}
+
+//         <Route
+//           path="/home"
+//           element={
+//             <RequireAuth>
+//               <Home />
+//             </RequireAuth>
+//           }
+//         />
+//         <Route
+//           path="/favorites"
+//           element={
+//             <PrivateRoutes>
+//               <Favorites />
+//             </PrivateRoutes>
+//           }
+//         />
+//         <Route
+//           path="/synopsis"
+//           element={
+//             <PrivateRoutes>
+//               <Synopsis />
+//             </PrivateRoutes>
+//           }
+//         />
+//         <Route
+//           path="/henry_m2"
+//           element={
+//             <PrivateRoutes>
+//               <Henry_m2 />
+//             </PrivateRoutes>
+//           }
+//         />
+//         <Route
+//           path="/tecnologies"
+//           element={
+//             <PrivateRoutes>
+//               <Tecnologies />
+//             </PrivateRoutes>
+//           }
+//         />
+//         <Route
+//           path="/about"
+//           element={
+//             <PrivateRoutes>
+//               <About />
+//             </PrivateRoutes>
+//           }
+//         />
+//         <Route path="*" element={<Error404 />} />
+//       </Routes>
+//     </>
+//   );
+// };
