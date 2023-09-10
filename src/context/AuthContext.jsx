24be85a235c7a36a693a2d1ebef98393512bf143
Armando Martinez <alemar.martinez16@gmail.com>
2023-09-09 23:43:38 -0300

    add metodos de autenticacion email y password

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,72 +1,42 @@
-// import React, { useContext, useState, useEffect, createContext } from "react";
-// import {
-//   GoogleAuthProvider,
-//   signInWithRedirect,
-//   onAuthStateChanged,
-//   signOut,
-// } from "firebase/auth";
-// import { auth } from "../components/firebase/firebase.js";
-
-// const AuthContext = createContext();
-// export const AuthContextProvider = ({ children }) => {
-//   const [user, setUser] = useState({});
-//   const googleSignIn = () => {
-//     const provider = new GoogleAuthProvider(); //Crear un nuevo proveedor de autenticación de Google
-//     signInWithRedirect(auth, provider); //Redirigir al usuario a la página de autenticación de Google
-//   };
-//   const logout = () => {
-//     signOut(auth);
-//   };
-//   //se usa el useEffect para que se ejecute cada vez que se renderiza el componente
-//   useEffect(() => {
-//     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-//       setUser(currentUser);
-//     });
-//     return () => {
-//       unsubscribe();
-//     };
-//   }, []); // en este caso solo se ejecuta una vez
-
-//   return (
-//     <AuthContext.Provider value={{ user, googleSignIn, logout }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
-
-// //Retorna el contexto
-// export const UserAuth = () => {
-//   return useContext(AuthContext);
-// };
-
-
-
-
-
-import React, { useContext, useState, useEffect, createContext } from "react";
+import { useContext, useState, useEffect, createContext } from "react";
+import PropTypes from "prop-types";
 import {
   GoogleAuthProvider,
   signInWithPopup,
   // signInWithRedirect,
   onAuthStateChanged,
   signOut,
-  setPersistence,
-  browserSessionPersistence,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
 } from "firebase/auth";
 import { auth } from "../components/firebase/firebase.js";
 
 const AuthContext = createContext();
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
-const googleSignIn = async () => {
-  try {
-    const provider = new GoogleAuthProvider();
-    await setPersistence(auth, browserSessionPersistence);
-    /* signInWithRedirect */ signInWithPopup(auth, provider);
-  } catch (error) {
-    // Manejar el error
-  }
-};
+  const googleSignIn = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      signInWithPopup(auth, provider);
+    } catch (error) {
+      console.log("Error al iniciar sesión en Google", error);
+      // Manejar el error
+    }
+  };
+  const signInWithEmail = async (email, password) => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const signUpWithEmail = async (email, password) => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const logout = () => {
     signOut(auth);
   };
@@ -81,7 +51,9 @@ const googleSignIn = async () => {
   }, []); // en este caso solo se ejecuta una vez
 
   return (
-    <AuthContext.Provider value={{ user, googleSignIn, logout }}>
+    <AuthContext.Provider
+      value={{ user, googleSignIn, logout, signInWithEmail, signUpWithEmail }}
+    >
       {children}
     </AuthContext.Provider>
   );
@@ -90,4 +62,7 @@ const googleSignIn = async () => {
 //Retorna el contexto
 export const UserAuth = () => {
   return useContext(AuthContext);
+};
+AuthContextProvider.propTypes = {
+  children: PropTypes.node.isRequired,
 };
