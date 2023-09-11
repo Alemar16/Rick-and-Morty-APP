8c97bf2792789ad2571c9a8d9b9d914607c328d1
Armando Martinez <alemar.martinez16@gmail.com>
2023-09-10 22:53:24 -0300

    Se configura en el formulario el input de "name"

--- src/pages/Login2.jsx
+++ src/pages/Login2.jsx
@@ -9,25 +9,27 @@ const Login2 = () => {
   //para autentizar con Google
   const { user, googleSignIn } = UserAuth();
   const navigate = useNavigate();
-  const iniciarSesion = async () => {
+  const handleGoogleSignIn = async (e) => {
+    e.preventDefault();
     try {
       await googleSignIn();
       console.log("Usuario a iniciado en su cuenta en Google");
     } catch (error) {
-      console.log(
-        "Error al iniciar seccion en la cunta de google desde login",
-        error
-      );
+      console.error("Error al iniciar sesión en Google", error);
     }
   };
+
   //para iniciar sesion con email y password
   const { signInWithEmail, signUpWithEmail } = UserAuth();
   //estado para almacenar datos del formulario
   const [formData, setFormData] = useState({
     email: "",
     password: "",
+    name: "",
   });
-  const [error, setError] = useState("");
+
+  /* const [error, setError] = useState(""); */
+
   //funcion para manejar cambios en los campos del formulario
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -42,31 +44,33 @@ const Login2 = () => {
     try {
       const { email, password } = formData;
       await signInWithEmail(email, password);
-      console.log('Usuario a iniciado en su cuenta');
+      /* console.log("Informacion del usuario", user); */
+      console.log("Usuario a iniciado en su cuenta");
       // Redirige al usuario a la página de inicio después del inicio de sesión exitoso
       navigate("/home");
     } catch (error) {
       console.error("Error al iniciar sesión:", error);
-      setError("Error al iniciar sesión. Verifica tus credenciales.");
+      /*  setError("Error al iniciar sesión. Verifica tus credenciales."); */
     }
   };
   // Función para manejar el registro con email y contraseña
   const handleSignUp = async (e) => {
     e.preventDefault();
     try {
-      const { email, password } = formData;
-      await signUpWithEmail(email, password);
-      console.log('Usuario a creado su cuenta');
+      const { email, password, name } = formData;
+      await signUpWithEmail(email, password, name);
+      /*   console.log("información del usuario registrado",user) */
+      console.log("Usuario a creado su cuenta");
       // Redirige al usuario a la página de inicio después del registro exitoso
       navigate("/home");
     } catch (error) {
-      console.error("Error al registrar usuario:", error);
+      console.error("Error al registrar usuario:");
     }
   };
 
   useEffect(() => {
     if (user != null) {
-      console.log(user);
+      /* console.log("usuario autenticado", user); */
       navigate("/home");
     }
   }, [user, navigate]);
@@ -200,7 +204,7 @@ const Login2 = () => {
               <button
                 className="btn btn-dark btn-floating mx-2"
                 title="Google"
-                onClick={iniciarSesion}
+                onClick={handleGoogleSignIn}
               >
                 <img src={googleIcon} alt="Google" style={{ width: "35px" }} />
               </button>
@@ -304,7 +308,7 @@ const Login2 = () => {
             </div>
 
             {/* identification Name input and Username input */}
-            {/* Email input */}
+            {/* name input */}
             <div className="input-group flex-nowrap mb-4">
               <span className="input-group-text" id="addon-wrapping">
                 <i className="fa-solid fa-user-large"></i>
@@ -313,7 +317,7 @@ const Login2 = () => {
                 type="text"
                 className="form-control"
                 placeholder="Name"
-                aria-label="Username"
+                aria-label="Nameame"
                 aria-describedby="addon-wrapping"
                 name="name" // Nombre del campo
                 value={formData.name} // Valor del campo desde el estado
@@ -330,7 +334,7 @@ const Login2 = () => {
                 type="text"
                 className="form-control"
                 placeholder="Email"
-                aria-label="Username"
+                aria-label="Email"
                 aria-describedby="addon-wrapping"
                 name="email" // Nombre del campo
                 value={formData.email} // Valor del campo desde el estado
@@ -391,7 +395,7 @@ const Login2 = () => {
                 className="btn btn-primary btn-block"
                 style={{ margin: "0 auto" }}
                 onClick={(e) => {
-                  if(!isChecked){
+                  if (!isChecked) {
                     alert("Debe aceptar los términos y condiciones");
                     return;
                   }
@@ -418,359 +422,3 @@ const Login2 = () => {
 };
 
 export default Login2;
-
-//  <div className="row mb-4">
-//    <div className="col">
-
-//      <div className="input-group flex-nowrap mb-1 me-1">
-//        <span className="input-group-text" id="addon-wrapping">
-//          <i class="fa-solid fa-user-large"></i>
-//        </span>
-//        <input
-//          type="text"
-//          className="form-control"
-//          placeholder="Name"
-//          aria-label="Username"
-//          aria-describedby="addon-wrapping"
-//        />
-//      </div>
-//    </div>
-//    <div className="col">
-
-//      <div className="input-group flex-nowrap mb-1">
-//        <span className="input-group-text" id="addon-wrapping">
-//          <i class="fa-solid fa-user-ninja"></i>
-//        </span>
-//        <input
-//          type="text"
-//          className="form-control"
-//          placeholder="Username"
-//          aria-label="Username"
-//          aria-describedby="addon-wrapping"
-//        />
-//      </div>
-//    </div>
-//  </div>;
-
-//========================================================================
-// import { useState } from "react";
-// import facebookIcon from "../../assets/socialIcons/facebook_icon.svg";
-// import googleIcon from "../../assets/socialIcons/google_icon.svg";
-// import githubIcon from "../../assets/socialIcons/github_icon.svg";
-
-// const Login2 = () => {
-//   //para intercambiar entre los componentes login y register
-//   const [activeTab, setActiveTab] = useState("login");
-//   const handleTabClick = (tab) => {
-//     setActiveTab(tab);
-//   };
-
-//   //para validar checkbox de terminos y condiciones
-//   const [isChecked, setIsChecked] = useState(false);
-//   const handleCheckboxChange = (event) => {
-//     setIsChecked(event.target.checked);
-//   };
-//   const handleFormSubmit = (event) => {
-//     event.preventDefault();
-//     if (!isChecked) {
-//       alert("Debe aceptar los términos y condiciones");
-//       return;
-//     }
-//   };
-
-//   //para validar checbox de remember me
-//   const [rememberMe, setRememberMe] = useState(false);
-//   const handleCheckboxChangeRememberMe = (event) => {
-//     setRememberMe(event.target.checked);
-//   };
-
-//   return (
-//     <div
-//       className="container mt-5 mb-4 bg-dark rounded"
-//       style={{
-//         maxWidth: "360px",
-//         minWidth: "230px",
-//         margin: "auto",
-//         paddingTop: "10px",
-//         boxShadow: "0 2px 4px rgba(0, 0, 0, 0.4)",
-//         border: "1px solid rgba(255, 255, 255, 0.1)",
-//       }}
-//     >
-//       {/* Pills navs */}
-//       <ul className="nav nav-pills nav-justified mb-3 " id="ex1" role="tablist">
-//         {/* Login */}
-//         <li className="nav-item" role="presentation">
-//           <a
-//             className={`nav-link ${activeTab === "login" ? "active" : ""}`}
-//             onClick={() => handleTabClick("login")}
-//             id="tab-login"
-//             data-mdb-toggle="pill"
-//             href="#pills-login"
-//             role="tab"
-//             aria-controls="pills-login"
-//             aria-selected={activeTab === "login"}
-//           >
-//             Login
-//           </a>
-//         </li>
-//         {/* Register */}
-//         <li className="nav-item" role="presentation">
-//           <a
-//             className={`nav-link ${activeTab === "register" ? "active" : ""}`}
-//             onClick={() => handleTabClick("register")}
-//             id="tab-register"
-//             data-mdb-toggle="pill"
-//             href="#pills-register"
-//             role="tab"
-//             aria-controls="pills-register"
-//             aria-selected={activeTab === "register"}
-//           >
-//             Register
-//           </a>
-//         </li>
-//       </ul>
-//       {/*  Pills navs*/}
-
-//       {/* Pills content */}
-//       <div className="tab-content">
-//         {/* Login */}
-//         <div
-//           className={`tab-pane fade ${
-//             activeTab === "login" ? "show active" : ""
-//           }`}
-//           id="pills-login"
-//           role="tabpanel"
-//           aria-labelledby="tab-login"
-//         >
-//           <form>
-//             <div className="text-center mb-3">
-//               <p>Sign in with:</p>
-
-//               {/* Facebook */}
-//               <button
-//                 className="btn btn-dark btn-floating mx-2"
-//                 title="Facebook"
-//               >
-//                 <img
-//                   src={facebookIcon}
-//                   alt="Facebook"
-//                   style={{ width: "35px" }}
-//                 />
-//               </button>
-//               {/* Google */}
-//               <button className="btn btn-dark btn-floating mx-2" title="Google">
-//                 <img src={googleIcon} alt="Google" style={{ width: "35px" }} />
-//               </button>
-//               {/* Github */}
-//               <button className="btn btn-dark btn-floating mx-2" title="Github">
-//                 <img src={githubIcon} alt="Github" style={{ width: "35px" }} />
-//               </button>
-//             </div>
-
-//             <p className="text-center">or:</p>
-
-//             {/* Email input */}
-//             <div className="input-group flex-nowrap mb-4">
-//               <span className="input-group-text" id="addon-wrapping">
-//                 <i className="fa-solid fa-envelope"></i>
-//               </span>
-//               <input
-//                 type="text"
-//                 className="form-control"
-//                 placeholder="Email"
-//                 aria-label="Username"
-//                 aria-describedby="addon-wrapping"
-//               />
-//             </div>
-
-//             {/* Password input */}
-//             <div className="input-group flex-nowrap mb-4">
-//               <span className="input-group-text" id="addon-wrapping">
-//                 <i className="fa-solid fa-key"></i>
-//               </span>
-//               <input
-//                 type="password"
-//                 className="form-control"
-//                 placeholder="Password"
-//                 aria-label="Password"
-//                 aria-describedby="addon-wrapping"
-//               />
-//             </div>
-
-//             {/*  2 column grid layout*/}
-//             <div className="row mb-4">
-//               <div className="col-md-6 d-flex justify-content-center">
-//                 {/* Checkbox */}
-//                 <div className="form-check mb-3 mb-md-0">
-//                   <input
-//                     className="form-check-input"
-//                     type="checkbox"
-//                     value=""
-//                     id="loginCheck"
-//                     checked={rememberMe}
-//                     onChange={handleCheckboxChangeRememberMe}
-//                   />
-//                   <label className="form-check-label" htmlFor="loginCheck">
-//                     Remember me
-//                   </label>
-//                 </div>
-//               </div>
-
-//               {/* Simple link */}
-//               <div className="col-md-6 d-flex justify-content-center">
-//                 <a href="#!">Forgot password?</a>
-//               </div>
-//             </div>
-
-//             {/* Submit button */}
-//             <div className="text-center">
-//               <button
-//                 type="submit"
-//                 className="btn btn-primary btn-block mb-3"
-//                 style={{ margin: "0 auto" }}
-//               >
-//                 Sign in
-//               </button>
-//             </div>
-
-//             {/* Register buttons */}
-//             {/* <div className="text-center">
-//               <p>
-//                 Not a member? <a href="#!">Register</a>
-//               </p>
-//             </div> */}
-//           </form>
-//         </div>
-
-//         {/* Register */}
-//         <div
-//           className={`tab-pane fade ${
-//             activeTab === "register" ? "show active " : ""
-//           }`}
-//           id="pills-register"
-//           role="tabpanel"
-//           aria-labelledby="tab-register"
-//           /* style={{maxHeight: "400px"}} */
-//         >
-//           <form onSubmit={handleFormSubmit}>
-//             <div className="text-center mb-3">
-//               <p>Sign in with:</p>
-
-//               {/* Facebook */}
-//               <button
-//                 className="btn btn-dark btn-floating mx-2"
-//                 title="Facebook"
-//               >
-//                 <img
-//                   src={facebookIcon}
-//                   alt="Facebook"
-//                   style={{ width: "35px" }}
-//                 />
-//               </button>
-//               {/* Google */}
-//               <button className="btn btn-dark btn-floating mx-2" title="Google">
-//                 <img src={googleIcon} alt="Google" style={{ width: "35px" }} />
-//               </button>
-//               {/* Github */}
-//               <button className="btn btn-dark btn-floating mx-2" title="Github">
-//                 <img src={githubIcon} alt="Github" style={{ width: "35px" }} />
-//               </button>
-//             </div>
-
-//             <p className="text-center">or:</p>
-
-// {/* Name input */}
-// {/* <div className="form-outline mb-4">
-//   <input type="text" id="registerName" className="form-control" />
-//   <label className="form-label" htmlFor="registerName">
-//     Name
-//   </label>
-// </div> */}
-
-// {/* Username input */}
-// {/* <div className="form-outline mb-4">
-//   <input
-//     type="text"
-//     id="registerUsername"
-//     className="form-control"
-//   />
-//   <label className="form-label" htmlFor="registerUsername">
-//     Username
-//   </label>
-// </div> */}
-
-//             {/* Email input */}
-//             <div className="input-group flex-nowrap mb-4">
-//               <span className="input-group-text" id="addon-wrapping">
-//                 <i className="fa-solid fa-envelope"></i>
-//               </span>
-//               <input
-//                 type="text"
-//                 className="form-control"
-//                 placeholder="Email"
-//                 aria-label="Username"
-//                 aria-describedby="addon-wrapping"
-//               />
-//             </div>
-
-//             {/* Password input */}
-//             <div className="input-group flex-nowrap mb-4">
-//               <span className="input-group-text" id="addon-wrapping">
-//                 <i className="fa-solid fa-key"></i>
-//               </span>
-//               <input
-//                 type="password"
-//                 className="form-control"
-//                 placeholder="Password"
-//                 aria-label="Password"
-//                 aria-describedby="addon-wrapping"
-//               />
-//             </div>
-
-//             {/* Repeat Password input */}
-//             {/* <div className="form-outline mb-4">
-//               <input
-//                 type="password"
-//                 id="registerRepeatPassword"
-//                 className="form-control"
-//               />
-//               <label className="form-label" htmlFor="registerRepeatPassword">
-//                 Repeat password
-//               </label>
-//             </div> */}
-
-//             {/* Checkbox */}
-//             <div className="form-check d-flex justify-content-center mb-4">
-//               <input
-//                 className="form-check-input me-2"
-//                 type="checkbox"
-//                 value=""
-//                 id="registerCheck"
-//                 checked={isChecked}
-//                 aria-describedby="registerCheckHelpText"
-//                 onChange={handleCheckboxChange}
-//               />
-//               <label className="form-check-label" htmlFor="registerCheck">
-//                 I have read and agree to the terms
-//               </label>
-//             </div>
-
-//             {/* Submit button */}
-//             <div className="text-center">
-//               <button
-//                 type="submit"
-//                 className="btn btn-primary btn-block mb-3"
-//                 style={{ margin: "0 auto" }}
-//               >
-//                 Sign in
-//               </button>
-//             </div>
-//           </form>
-//         </div>
-//       </div>
-//       {/* Pills content */}
-//     </div>
-//   );
-// };
-
-// export default Login2;
